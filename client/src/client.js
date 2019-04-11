e1cdd78b4545a555e14b24c4f1289b02cbb60c36
pattycakelol <pleung1415@gmail.com>
2019-04-10 18:06:51 -0700

    handles disconnects & stores messages

--- client/src/client.js
+++ client/src/client.js
@@ -11,21 +11,28 @@ const chatMessage = (messageObj) => {
     // chat.scrollTop = chat.scrollHeight;
 };
 
-const onChatSend = (e) => {
-    e.preventDefault();
-    const input = document.querySelector('#chatMessage');
-    const text = input.value;
-    input.value = '';
-    socket.emit('message', {
-        type: 'chat',
-        text: text
-    });
-};
+// const onChatSend = (e) => {
+//     e.preventDefault();
+//     const input = document.querySelector('#chatMessage');
+//     const text = input.value;
+//     input.value = '';
+//     socket.emit('message', {
+//         type: 'chat',
+//         text: text
+//     });
+// };
 
-// socket.on('message', writeEvent);
 socket.on('message', chatMessage);
 
 // socket.emit('serverLog', "test");
 
 // when message is sent though chat input, emit messageto server
-document.querySelector('#chat-form').addEventListener('submit', onChatSend);
+// document.querySelector('#chat-form').addEventListener('submit', onChatSend);
+$('#chat-form').submit( (event) => {
+    socket.emit('message', {
+        type: 'chat',
+        text: $('#chat-message').val()
+    });
+    $('#chat-message').val('');
+    event.preventDefault();
+});
